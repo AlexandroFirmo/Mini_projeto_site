5bd13fae9a7e69bcc034eda48b291f1090f9d7f1
AlexandroFirmo <alexandro_firmo@hotmail.com>
2017-04-09 19:32:22 -0300

    Update rodape.php

--- meusite/rodape.php
+++ meusite/rodape.php
@@ -1,6 +1,6 @@
 <div class="container">
 	<hr>
 	<footer class="container-fluid text-center">
-		<p>meuSite.com - allcopy </p>
+		<p><a href="index.php">meuSite.com</a> Copyrigh </p>
 	</footer>
 </div>
